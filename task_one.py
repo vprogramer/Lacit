31f6b8637d64288703ef42e87164f4ef152bfbc5
vlad <vlad.bond.1998@mail.ru>
2020-01-25 19:40:21 +0300

    New

--- task_one.py
+++ task_one.py
@@ -3,6 +3,10 @@ import cv2
 faceCascadeFrontal = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 facecascadeProfile = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
+additive = 10 # Добавка
+direction = ""
+previous_face = [[0,0,0,0]] # Начальное положение
+
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
@@ -19,14 +23,27 @@ while True:
         cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-    cv2.imshow('video', img)
 
- #   if previous_face[0] < x and previous_face[1] < y and previous_face[2] < w and previous_face[3] < h:
-  #      print("Lower")
-    #cv2.putText(img, direction, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    if faces is not ():
-        previous_face = faces
+        #Определение направления
+        if faces is not ():
+            if previous_face[0][0] + additive < faces[0][0]:
+                direction += " Left"
+            if previous_face[0][0] - additive > faces[0][0]:
+                direction += " Right"
+            if (previous_face[0][0] + previous_face[0][2] + additive) < (faces[0][0] + faces[0][2]) and (
+                    previous_face[0][1] + previous_face[0][3] + additive) < (faces[0][1] + faces[0][3]):
+                direction += " Bigger"
+            if (previous_face[0][0] + previous_face[0][2] - additive) > (faces[0][0] + faces[0][2]) and (
+                    previous_face[0][1] + previous_face[0][3] - additive) > (faces[0][1] + faces[0][3]):
+                direction += " Smaller"
+            previous_face = faces
+        if direction is not "": #Последнее направление движение будет выводиться до появления следующего
+            pre_dir = direction
+        #print(direction)
+        cv2.putText(img, pre_dir, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    resized_img = cv2.resize(img, (1000, 700))
+    cv2.imshow('video', resized_img)
+    direction = ""
     k = cv2.waitKey(30) & 0xff
     if k == 27:  # press 'ESC' to quit
         break
